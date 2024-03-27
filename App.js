96f629395accab5a673a79f17afea4f0fee7f01d
UMANG KIRIT LODAYA <lodayaumang71@gmail.com>
2024-03-27 20:43:48 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, ImageBackground } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const localImage = require("./assets/adaptive-icon.png")
+
+export default App = () => {
+  return <View style={{ flex: 1, padding: 60, backgroundColor: "plum" }}>
+    {/* <View style={{ backgroundColor: "lightblue", height: 200, width: 200 }}></View> */}
+    {/* <View style={{ backgroundColor: "lightgreen", height: 200, width: 200 }}></View> */}
+    {/* <Text>
+      <Text style={{ color: "green" }}>Hello</Text> World
+    </Text> */}
+
+    {/* <Image source={localImage} style={{ height: 300, width: 300 }} /> */}
+    {/* <Image source={{ uri: "https://picsum.photos/300" }} style={{ height: 300, width: 300 }} /> */}
+    <ImageBackground source={localImage} style={{ flex: 1 }}>
+      <Text>
+        Image Text
+      </Text>
+    </ImageBackground>
+
+  </View>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
